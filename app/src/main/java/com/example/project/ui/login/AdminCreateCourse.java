f8a4f33bd4b976cada14062cf2b02cf579113328
SebastianCa01 <97400037+SebastianCa01@users.noreply.github.com>
2022-06-14 18:41:02 -0400

    Update AdminCreateCourse.java

--- app/src/main/java/com/example/project/ui/login/AdminCreateCourse.java
+++ app/src/main/java/com/example/project/ui/login/AdminCreateCourse.java
@@ -7,6 +7,19 @@ import android.os.Bundle;
 import com.example.project.R;
 
 public class AdminCreateCourse extends AppCompatActivity {
+    private static final String TABLE_NAME = "courses";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_PRODUCT_NAME = "name";
+    private static final String COLUMN_PRODUCT_PRICE = "code";
+    private static final String DATABASE_NAME = "adminCreateCourse.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public adminCreateCourse(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
