e43a14052212021ff6931d5f778f194b8e99babe
SebastianCa01 <97400037+SebastianCa01@users.noreply.github.com>
2022-06-14 18:23:38 -0400

    Update AdminActivity.java

--- app/src/main/java/com/example/project/ui/login/AdminActivity.java
+++ app/src/main/java/com/example/project/ui/login/AdminActivity.java
@@ -6,10 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.project.R;
+
+import java.util.ArrayList;
 
 import com.example.project.R;
 
 public class AdminActivity extends AppCompatActivity {
+     EditText courseName, courseCode;
+     ListView courseListView;
+
+    ArrayList<String> courseList;
+    ArrayAdapter adapter;
+    AdminCreateCourse adminCreateCourse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,16 @@ public class AdminActivity extends AppCompatActivity {
         final Button deleteC = findViewById(R.id.deleteCourseButton);
         final Button manageB = findViewById(R.id.manage_user_account);
         manageB.setEnabled(false);
+        
+        courseList = new ArrayList<>();
+
+        courseName = (EditText)findViewById(R.id.courseName);
+        courseCode = (EditText)findViewById(R.id.courseCode);
+        
+         courseListView = findViewById(R.id.courseListView);
+
+        adminCreateCourse = new AdminCreateCourse(this);
+        
         createC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,5 +67,19 @@ public class AdminActivity extends AppCompatActivity {
                 startActivity(deleteCourseIntent);
             }
         });
+        private void createCourse(View view){
+            AdminCreateCourse admincreateCourse = new AdminCreateCourse(this);
+
+            int code = Integer.parseInt(courseCode.getText().toString());
+
+            Course course = new Course(courseName.getText().toString(), code);
+
+            AdminCreateCourse.createCourse(course);
+
+
+            courseName.setText("");
+            courseCode.setText("");
+        }
+
     }
 }
