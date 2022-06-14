4b6b97498d2eb50622ff5280311f93baadba17a2
sshar259 <106174806+sshar259@users.noreply.github.com>
2022-06-14 14:16:09 -0400

    firebase setup and new gui elements

---
+++ app/src/main/java/com/example/project/ui/login/AdminEditCourseActivity.java
@@ -0,0 +1,16 @@
+package com.example.project.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project.R;
+
+public class AdminEditCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_edit_course);
+    }
+}
